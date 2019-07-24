6eecf8f866662cfa27fbbda0e61941a9d7653323
sherif <negm@rochester.edu>
2019-07-23 20:00:17 -0400

    fasta formated refrence checker

--- Fasta_splitter.sh
+++ Fasta_splitter.sh
@@ -2,14 +2,32 @@
 
 Fasta_file=$1
 
+Start=`cat -A $Fasta_file  | head -c1`
+end=`cat -A $Fasta_file  | tail -c2`
 
+if [[ $Start != '>' ]]; then 
+ 
+echo "file dont appear to be in fasta format. it doesnt start with >"
+
+
+exit 1 
+
+fi 
+
+if [[ $end != '$' ]]; then 
+
+
+echo "Please make sure the file is unix formated. Which means it ends with an empty line. You can use tool like dos2unix to make sure it is"
+exit 1 
+
+fi 
 
 while read line
 do
     if [[ ${line:0:1} == '>' ]]
     then
-	File_name=`tr '> 	<.,:#"/\|?*' '_' <<<"$line"`
-	File_name_nofa=`tr '> 		<:#",/\|?*' '_' <<<"$line"`
+	File_name=`tr '> 	\\<.,:#"/\|?*' '_' <<<"$line"`
+	File_name_nofa=`tr '> 		<:#",/\\|?*' '_' <<<"$line"`
 #	File_name=$line
 	
 	File_extention=".fa"
