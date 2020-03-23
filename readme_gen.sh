f0d465fd37d4269b4b3e8493436c0d09706a1444
HounerX <negm@rochester.edu>
2020-03-23 18:02:18 -0400

    more fixes

--- readme_gen.sh
+++ readme_gen.sh
@@ -3,7 +3,7 @@
 echo '								************************Repeat_Profiler***********************' > ReadMe.txt
 
 echo "The tool indexes input reads and refers to them internally as numbers. In most cases it converts indexes back your read names,">> ReadMe.txt
-echo "however, some output will refer to your your reads by index number (i.e., the subfolders within each reference sequence output folder).">> ReadMe.txt
+echo "however, some output will refer to your reads by index number (i.e., the subfolders within each reference sequence output folder).">> ReadMe.txt
 echo "Use the table below to translate index numbers back to your input reads as needed.">> ReadMe.txt
 echo '		' >> ReadMe.txt
 
