75dba5dc0ed1d0b162f7e41eda5eddc4285bc96f
johnssproul <51719438+johnssproul@users.noreply.github.com>
2020-05-12 13:31:23 -0400

    Update readme_gen.sh

--- scripts/readme_gen.sh
+++ scripts/readme_gen.sh
@@ -4,8 +4,8 @@ echo '								************************Repeat_Profiler***********************' > 
 
 echo "The pipeline indexes input reads and refers to them internally as numbers. In most cases it converts indexes back your read names,">> ReadMe.txt
 echo "however, some output will refer to your reads by index number (i.e., the subfolders within each reference sequence output folder).">> ReadMe.txt
-echo "Refer to index_conv.txt in the 'temp' folder associated with the run to translate index numbers back to your input reads names as needed.">> ReadMe.txt
+echo "Refer to the table below to translate index numbers back to your input reads names as needed.">> ReadMe.txt
 echo '		' >> ReadMe.txt
 
-#echo 'Index -> Reads:' >> ReadMe.txt
+echo 'Index -> Reads:' >> ReadMe.txt
 echo '		' >> ReadMe.txt
